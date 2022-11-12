690e91401654f631a32c6ca3ba6b3bfd12254252
Isabella <isabellamcapriotti@gmail.com>
2022-11-12 14:58:06 -0600

    Fixed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ElevationComponent } from './elevation/elevation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ElevationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/elevation/elevation.component.ts
+++ src/app/elevation/elevation.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'elevation',
-    templateUrl: './elevation.component.ts',
+    templateUrl: './elevation.component.html',
     styleUrls: ['./elevation.component.css']    
 })
 
-export class ElevationComponent{
+export class ElevationComponent implements OnInit{
+
+    static readonly dataJsonUrl : string = "https://www.usbr.gov/uc/water/hydrodata/reservoir_data/921/json/49.json";
+
+    elevationDataJson : string = "";
 
     constructor(private http : HttpClient){}
-    
+
+    ngOnInit(){
+        this.readElevationData().subscribe((waterData : string) => {
+            console.log('got water data');
+            console.log(waterData);
+        });
+    }
+
+    readElevationData(){
+        return this.http.get<string>(ElevationComponent.dataJsonUrl);
+    }
 }
