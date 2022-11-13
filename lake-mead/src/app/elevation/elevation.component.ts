b86babc6d6f46899de01acf11e65f68654fbf282
Heidi Fehr <heidi.a.fehr@gmail.com>
2022-11-13 08:50:39 -0600

    Got the elevation to truncate to 2 decimal places

--- lake-mead/src/app/elevation/elevation.component.ts
+++ lake-mead/src/app/elevation/elevation.component.ts
@@ -102,7 +102,7 @@ export class ElevationComponent implements OnInit{
         // 1000: 10%, power generation limit
         
         // Get elevation for current year 
-        this.currElevation = this.elevationDataByYearAveraged.get(this.sliderYearValue);
+        this.currElevation = parseFloat(this.elevationDataByYearAveraged.get(this.sliderYearValue)?.toFixed(2) ?? "0.0");
         console.log('current elevation: ' + this.currElevation);
 
         // Get water height corresponding to elevation
